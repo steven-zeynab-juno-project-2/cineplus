b1052698ba79e63008c3837843b9473ee2c99f02
Steven Chen <steven.chen.121@gmail.com>
2021-09-18 18:25:15 -0400

    Added JS logic for getting genres and getting movies based on selected genre.

--- script.js
+++ script.js
@@ -12,12 +12,57 @@
 
 const movieApp = {};
 
+
+movieApp.apiURL = `https://api.themoviedb.org/3/`;
+movieApp.apiKey = `589df13bf2644ed869e616fad6d941ce`;
+
+movieApp.searchButton = document.querySelector('.searchButton');
+
 movieApp.getGenres = () => {
-    fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=589df13bf2644ed869e616fad6d941ce`)
-    .then(response => response.json())
-    .then((jsonData) => {
-        console.log(jsonData.genres[0]);
-    });
+
+    const url = new URL(`${movieApp.apiURL}genre/movie/list`);
+    url.search = new URLSearchParams({
+        // pass in our API key as a parameter
+        api_key: movieApp.apiKey
+    })
+
+    fetch(url)
+        .then(response => response.json())
+        .then((jsonData) => {
+            const genres = jsonData.genres; // array
+            const selectGenres = document.querySelector('#genres');
+            genres.forEach((genre) => {
+                const optionItem = document.createElement('option');
+                optionItem.value = genre.id;
+                optionItem.textContent = genre.name;
+                selectGenres.append(optionItem);
+            });
+        });
 }
 
-movieApp.getGenres ();
+movieApp.getMovies = (genre) => {
+    const url = new URL(`${movieApp.apiURL}discover/movie`);
+    url.search = new URLSearchParams({
+        api_key: movieApp.apiKey,
+        with_genres: genre
+    });
+
+    fetch(url)
+        .then(response => response.json())
+        .then((jsonData) => {
+            console.log(jsonData);
+        });
+};
+
+movieApp.init = () => {
+
+    movieApp.getGenres();
+
+    movieApp.searchButton.addEventListener('click', () => {
+        const selectedOption = document.querySelector('#genres option:checked').value;
+
+        movieApp.getMovies(selectedOption);
+    });
+};
+
+movieApp.init();
