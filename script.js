4d15cf4167da732a7c00674861167e9bef9b91b2
Steven Chen <steven.chen.121@gmail.com>
2021-09-20 19:38:22 -0400

    Adding SCSS partials and initial CSS stylings.

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-// const testAPI = () => {
-
-//     // &sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=1990-01-01&primary_release_date.lte=1999-12-31
-//     fetch(`https://api.themoviedb.org/3/discover/movie?api_key=589df13bf2644ed869e616fad6d941ce&language=en-US&vote_average.gte=6&with_genres=28,35`)
-//         .then(response => response.json())
-//         .then((jsonData) => {
-//             console.log(jsonData);
-//         });
-//     // /genre/movie/list
-// };
-// testAPI();
+// &sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=1990-01-01&primary_release_date.lte=1999-12-31
 
 const movieApp = {};
-
 
 movieApp.apiURL = `https://api.themoviedb.org/3`;
 movieApp.imgURL = `https://image.tmdb.org/t/p/w500`;
 movieApp.apiKey = `589df13bf2644ed869e616fad6d941ce`;
 
-movieApp.searchButton = document.querySelector('.searchButton');
+movieApp.init = () => {
+    movieApp.setupEventListener();
+    movieApp.getGenres();
+};
+
+movieApp.setupEventListener = () => {
+    searchButton = document.querySelector('.searchButton');
+
+    searchButton.addEventListener('click', () => {
+        const selectedOption = document.querySelector('#genres option:checked').value;
+
+        movieApp.getMovies(selectedOption);
+    });
+};
 
 movieApp.getGenres = () => {
 
@@ -56,30 +58,27 @@ movieApp.getMovies = (genre) => {
         });
 };
 
-movieApp.init = () => {
-
-    movieApp.getGenres();
-
-    movieApp.searchButton.addEventListener('click', () => {
-        const selectedOption = document.querySelector('#genres option:checked').value;
-
-        movieApp.getMovies(selectedOption);
-    });
-};
-
-movieApp.init();
-
-
-movieApp.displayMovies = (movies) => { 
+movieApp.displayMovies = (movies) => {
     const galleryList = document.querySelector('.galleryList');
     movies.forEach((movie) => {
         const title = movie.title;
         const poster = movie.poster_path;
-        const liItem = document.createElement('li');
-        liItem.innerHTML = `
-        <p>${title}</p>
-        <img src='${movieApp.imgURL}${poster}'>`
-        galleryList.append(liItem);
-        
+        const listItem = document.createElement('li');
+
+        listItem.classList.add('galleryItem');
+
+        listItem.innerHTML = `
+        <div class="movie">
+            <img src='${movieApp.imgURL}${poster}'>
+            <p>${title}</p>
+        </div>`
+
+        galleryList.append(listItem);
+
     });
 }
+
+movieApp.init();
+
+
+
