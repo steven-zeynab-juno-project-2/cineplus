4266f59c8c1b0481902de361bfce03255f7789c3
Steven Chen <steven.chen.121@gmail.com>
2021-09-20 20:47:14 -0400

    Adding menu functionality.

--- script.js
+++ script.js
@@ -7,18 +7,37 @@ movieApp.imgURL = `https://image.tmdb.org/t/p/w500`;
 movieApp.apiKey = `589df13bf2644ed869e616fad6d941ce`;
 
 movieApp.init = () => {
-    movieApp.setupEventListener();
     movieApp.getGenres();
+    movieApp.setupEventListener();
 };
 
 movieApp.setupEventListener = () => {
-    searchButton = document.querySelector('.searchButton');
+    const searchButton = document.querySelector('.searchButton');
 
     searchButton.addEventListener('click', () => {
-        const selectedOption = document.querySelector('#genres option:checked').value;
+        // const selectedOption = document.querySelector('#genres option:checked').value;
 
-        movieApp.getMovies(selectedOption);
+        movieApp.getMovies('28');
     });
+
+    // added 
+    // const buttons = document.querySelectorAll('.genre');
+    // const hide = document.querySelector('.hide');
+    // const genreList = document.querySelector('.genreList');
+
+    // buttons.forEach((button) => {
+    //     console.log(this);
+    //     button.addEventListener('click', function () {
+    //         this.classList.toggle('chosen');
+    //         console.log(this);
+    //     });
+    // });
+
+    // hide.addEventListener('click', function () {
+    //     genreList.classList.toggle('hidden');
+    // });
+    // added
+
 };
 
 movieApp.getGenres = () => {
@@ -33,14 +52,19 @@ movieApp.getGenres = () => {
         .then(response => response.json())
         .then((jsonData) => {
             const genres = jsonData.genres; // array
-            const selectGenres = document.querySelector('#genres');
+            const selectGenres = document.querySelector('.genreList');
             genres.forEach((genre) => {
-                const optionItem = document.createElement('option');
-                optionItem.value = genre.id;
-                optionItem.textContent = genre.name;
-                selectGenres.append(optionItem);
+                const listItem = document.createElement('li');
+                const genreButton = document.createElement('button');
+
+                genreButton.classList.add('genre');
+                genreButton.textContent = genre.name;
+                listItem.append(genreButton);
+                selectGenres.append(listItem);
             });
         });
+
+    
 }
 
 movieApp.getMovies = (genre) => {
@@ -67,11 +91,13 @@ movieApp.displayMovies = (movies) => {
 
         listItem.classList.add('galleryItem');
 
+
         listItem.innerHTML = `
         <div class="movie">
             <img src='${movieApp.imgURL}${poster}'>
             <p>${title}</p>
-        </div>`
+        </div>
+        `
 
         galleryList.append(listItem);
 
